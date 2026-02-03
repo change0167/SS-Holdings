eea6b952463a57d7961d540f011ea1c291d971bb
change0167 <109894202+change0167@users.noreply.github.com>
2026-02-03 18:07:40 +0530

    Update strapi.ts
    Updated the strapi file to be production level safe

--- src/api/strapi.ts
+++ src/api/strapi.ts
@@ -1,6 +1,24 @@
-const STRAPI_URL = "http://localhost:1337";
+//const STRAPI_URL = "http://localhost:1337";
+
+//export async function fetchFromStrapi(endpoint: string) {
+ // const res = await fetch(`${STRAPI_URL}/api/${endpoint}?populate=*`);
+
+  //if (!res.ok) {
+    //throw new Error("Failed to fetch from Strapi");
+  //}
+
+  //const json = await res.json();
+  //return json.data ?? [];
+//}
+const STRAPI_URL = import.meta.env.VITE_STRAPI_URL || "";
 
 export async function fetchFromStrapi(endpoint: string) {
+  // Backend not connected yet
+  if (!STRAPI_URL) {
+    console.warn("VITE_STRAPI_URL not set");
+    return [];
+  }
+
   const res = await fetch(`${STRAPI_URL}/api/${endpoint}?populate=*`);
 
   if (!res.ok) {
